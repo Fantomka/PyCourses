529ccd54b5abe41f56dc110802bd2309a93df8ab
Fantomka <43787512+Fantomka@users.noreply.github.com>
2019-03-04 22:48:12 +0300

    Delete NOD.py

--- NOD.py
+++
@@ -1,4 +0,0 @@
-def gcd(a: int, b: int) -> int:
-    for i in range(min(a, b), 0, -1): #идем от меньшего числа чтобы избежать лищних итераций
-        if (a % i) == 0 and (b % i) == 0:
-            return i
