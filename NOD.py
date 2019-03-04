d4b4963dde32b369a1d817e14b5436d51ce143e7
Fantomka <43787512+Fantomka@users.noreply.github.com>
2019-03-04 23:01:08 +0300

    Update NOD.py

--- NOD.py
+++ NOD.py
@@ -5,6 +5,6 @@ def gcd(a: int, b: int) -> int:
         return 0
     elif a != 0 and b == 0:
         return a
-    for i in range(min(a, b), -1, -1): #идем от меньшего числа чтобы избежать лищних итераций
+    for i in range(min(a, b), 0, -1): #идем от меньшего числа чтобы избежать лищних итераций
         if (a % i) == 0 and (b % i) == 0:
             return i
