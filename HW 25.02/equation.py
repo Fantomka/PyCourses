f795b5b7ea4b66590093fc3b9f93fd6e504db247
Fantomka <43787512+fantomka@users.noreply.github.com>
2019-04-30 14:48:17 +0300

    Адаптация заданий под обложение тестами

--- HW 25.02/equation.py
+++ HW 25.02/equation.py
@@ -1,21 +1,26 @@
 from math import sqrt
+import sys
 
 
-a = float(input("ВВедите коэффициент A: "))
-b = float(input("ВВедите коэффициент B: "))
-c = float(input("ВВедите коэффициент C: "))
+def equation(a, b, c):
+    if a != 0.0 and b != 0.0 and c != 0.0:
+        discriminant = b * b - 4 * a * c
+        if discriminant == 0.0:
+            return -b / (2 * a)
+        elif discriminant > 0:
+            return (-b - sqrt(discriminant)) / (2 * a), (-b + sqrt(discriminant)) / (2 * a)
+    elif a == 0.0 and b != 0.0:
+        return -(c / b)
+    elif a != 0.0 and b == 0.0:
+        if c < 0.0:
+            return [(-sqrt(-c / a)), (sqrt(-c / a))]
+        elif c == 0:
+            return 0
 
 
-if a != 0.0 and b != 0.0 and c != 0.0:
-    discriminant = b * b - 4 * a * c
-    if discriminant == 0.0:
-        print(f"X1:{-b / (2 * a)}")
-    elif discriminant > 0:
-        print(f"X1:{(-b - sqrt(discriminant)) / (2 * a)} X2:{(-b + sqrt(discriminant)) / (2 * a)}")
-elif a == 0.0 and b != 0.0:
-    print(f"X1:{-(c / b)}")
-elif a != 0.0 and b == 0.0:
-    if c > 0.0:
-        print(f"X1:{-sqrt(-c / a)} X2:{sqrt(-c / a)}")
-    elif c == 0:
-        print(f"X1:0")
+if __name__ == '__main__':
+    print(sys.path)
+    A = float(input("ВВедите коэффициент A: "))
+    B = float(input("ВВедите коэффициент B: "))
+    C = float(input("ВВедите коэффициент C: "))
+    print(equation(A, B, C))
